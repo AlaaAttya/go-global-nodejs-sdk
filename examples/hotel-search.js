5d9c5329725ba6018f451b5348df63086bc3835a
AlaaAttya <alaa.attya91@gmail.com>
2017-12-23 00:20:42 +0200

    json data transformer

--- examples/hotel-search.js
+++ examples/hotel-search.js
@@ -5,6 +5,21 @@ const credentials = {
     agency: '1520350',
 };
 const goglobal = new GoGlobal(credentials, 'http://xml.qa.goglobal.travel/xmlwebservice.asmx');
-const data =  goglobal.getHotelClient().search({}).then((dd) => {
+const data =  goglobal.getHotelClient().search({
+    'checkInDate': '',
+    'checkoutDate': '',
+    'cityId' : '',
+    'rooms': [
+        {
+            'adults': 2,
+            'kids': 1
+        },
+        {
+            'adults': 3,
+            'kids': 2
+        }
+    ]
+
+}).then((dd) => {
     console.log(dd);
 });
