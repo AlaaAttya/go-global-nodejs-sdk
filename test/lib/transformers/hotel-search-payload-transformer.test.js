a4f1687f919386a26281350b11b3806c8d5abc3b
AlaaAttya <alaa.attya91@gmail.com>
2018-01-03 21:40:31 +0200

    add unit tests to the xml transformer

--- test/lib/transformers/hotel-search-payload-transformer.test.js
+++ test/lib/transformers/hotel-search-payload-transformer.test.js
@@ -6,7 +6,7 @@ describe('should have valid xml payload', () => {
         const searchParam = {
             'checkInDate': '2017-09-11',
             'checkoutDate': '2017-09-15',
-            'cityId' : 123,
+            'cityId': 123,
             'rooms': []
         };
 
@@ -51,50 +51,50 @@ describe('should have valid xml payload', () => {
     });
 
     it('should return valid xml string', () => {
-       const jsonData = {
-           Root: {
-               Header: {
-                   Agency: "123",
-                   User: 'alaa',
-                   Password: '11232342fgtrh',
-                   Operation: 'HOTEL_SEARCH_REQUEST',
-                   OperationType: 'Request',
-               },
-               Main: {
-                   '@Version': 1,
-                   '@ResponseFormat': 'JSON',
-                   SortOrder: 1,
-                   FilterPriceMin: 1,
-                   MaxResponses: 10, // limit the number of responses
-                   ArrivalDate: '2017-09-11',
-                   CityCode: 123,
-                   Nights: 4,
-                   Rooms: {
-                       Room: [],
-                   },
-                   Hotels: {
-                       HotelId: [],
-                   },
-               },
-           }
-       };
+        const jsonData = {
+            Root: {
+                Header: {
+                    Agency: '123',
+                    User: 'alaa',
+                    Password: '11232342fgtrh',
+                    Operation: 'HOTEL_SEARCH_REQUEST',
+                    OperationType: 'Request',
+                },
+                Main: {
+                    '@Version': 1,
+                    '@ResponseFormat': 'JSON',
+                    SortOrder: 1,
+                    FilterPriceMin: 1,
+                    MaxResponses: 10, // limit the number of responses
+                    ArrivalDate: '2017-09-11',
+                    CityCode: 123,
+                    Nights: 4,
+                    Rooms: {
+                        Room: [],
+                    },
+                    Hotels: {
+                        HotelId: [],
+                    },
+                },
+            },
+        };
 
-       const encodedXmlString = '&lt;Root&gt;&lt;Header&gt;&lt;Agency/&gt;&lt;User/&gt;&lt;Password/&gt;&lt;Operation&gt;HOTEL_SEARCH_REQUEST&lt;/Operation&gt;&lt;OperationType&gt;Request&lt;/OperationType&gt;&lt;/Header&gt;&lt;Main Version=&quot;2&quot; ResponseFormat=&quot;JSON&quot;&gt;&lt;SortOrder&gt;1&lt;/SortOrder&gt;&lt;FilterPriceMin&gt;1&lt;/FilterPriceMin&gt;&lt;MaxResponses&gt;10&lt;/MaxResponses&gt;&lt;ArrivalDate/&gt;&lt;CityCode/&gt;&lt;Nights&gt;4&lt;/Nights&gt;&lt;Rooms&gt;&lt;Room/&gt;&lt;/Rooms&gt;&lt;Hotels&gt;&lt;HotelId/&gt;&lt;/Hotels&gt;&lt;/Main&gt;&lt;/Root&gt;';
-       const expectedXmlString = `${'<?xml version="1.0" encoding="utf-8"?>\n' +
-           '<soap:Envelope\n' +
-           '\txmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"\n' +
-           '\txmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n' +
-           '\txmlns:xsd="http://www.w3.org/2001/XMLSchema">\n' +
-           '\t<soap:Body>\n' +
-           '\t\t<MakeRequest\n' +
-           '\t\t\txmlns="http://www.goglobal.travel/">\n' +
-           '\t\t\t<requestType>11</requestType>\n' +
-           '\t\t\t<xmlRequest>'}${encodedXmlString}` +
-           '\t\t\t</xmlRequest>\n' +
-           '\t\t</MakeRequest>\n' +
-           '\t</soap:Body>\n' +
-           '</soap:Envelope>';
+        const encodedXmlString = '&lt;Root&gt;&lt;Header&gt;&lt;Agency/&gt;&lt;User/&gt;&lt;Password/&gt;&lt;Operation&gt;HOTEL_SEARCH_REQUEST&lt;/Operation&gt;&lt;OperationType&gt;Request&lt;/OperationType&gt;&lt;/Header&gt;&lt;Main Version=&quot;2&quot; ResponseFormat=&quot;JSON&quot;&gt;&lt;SortOrder&gt;1&lt;/SortOrder&gt;&lt;FilterPriceMin&gt;1&lt;/FilterPriceMin&gt;&lt;MaxResponses&gt;10&lt;/MaxResponses&gt;&lt;ArrivalDate/&gt;&lt;CityCode/&gt;&lt;Nights&gt;4&lt;/Nights&gt;&lt;Rooms&gt;&lt;Room/&gt;&lt;/Rooms&gt;&lt;Hotels&gt;&lt;HotelId/&gt;&lt;/Hotels&gt;&lt;/Main&gt;&lt;/Root&gt;';
+        const expectedXmlString = `${'<?xml version="1.0" encoding="utf-8"?>\n' +
+            '<soap:Envelope\n' +
+            '\txmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"\n' +
+            '\txmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n' +
+            '\txmlns:xsd="http://www.w3.org/2001/XMLSchema">\n' +
+            '\t<soap:Body>\n' +
+            '\t\t<MakeRequest\n' +
+            '\t\t\txmlns="http://www.goglobal.travel/">\n' +
+            '\t\t\t<requestType>11</requestType>\n' +
+            '\t\t\t<xmlRequest>'}${encodedXmlString}` +
+            '\t\t\t</xmlRequest>\n' +
+            '\t\t</MakeRequest>\n' +
+            '\t</soap:Body>\n' +
+            '</soap:Envelope>';
 
-       expect(transformSearchPayload(jsonData)).toEqual(expectedXmlString);
+        expect(transformSearchPayload(jsonData)).toEqual(expectedXmlString);
     });
 });
